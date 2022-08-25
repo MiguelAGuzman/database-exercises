98b5281a15dc3c7ed467e5a4cccb388a7017fa9f
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-08-25 13:31:59 -0500

    db ex

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50) NOT NULL,
+                        name  VARCHAR(50) NOT NULL,
+                        release_date YEAR NOT NULL,
+                        sales FLOAT NOT NULL,
+                        genre VARCHAR(50) NOT NULL,
+                        PRIMARY KEY (id)
+);
