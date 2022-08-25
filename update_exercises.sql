cb53e5010cdc967f765a7391e1be67b1691b7bf5
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-08-25 16:55:10 -0500

    update exercise select

---
+++ update_exercises.sql
@@ -0,0 +1,7 @@
+SELECT * FROM albums AS name;
+SELECT * FROM albums AS release_date < 1980;
+SELECT * FROM albums AS artist = 'Michael Jackson';
+
+UPDATE albums
+SET sales = sales * 10;
+WHERE sales;
