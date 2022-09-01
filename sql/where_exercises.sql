96824a5e40c422fcffc6bf96f2e8002fccd4be46
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-09-01 14:24:31 -0500

    where ex

--- sql/where_exercises.sql
+++ sql/where_exercises.sql
@@ -1,3 +1,39 @@
-select first_name
+select *
 from employees
 where first_name in ('Irena', 'Vidya', 'Maya');
+
+select *
+from employees
+where last_name like 'E%';
+
+select *
+from employees
+where last_name like '%q%';
+
+select *
+from employees
+where first_name = 'Irena'
+or first_name = 'Vidya'
+or first_name = 'Maya';
+
+select *
+from employees
+where first_name = 'Irena'
+   or first_name = 'Vidya'
+   or first_name = 'Maya'
+and gender = 'M';
+
+select *
+from employees
+where last_name like 'E%'
+or last_name like '%E';
+
+select *
+from employees
+where last_name like 'E%'
+   and last_name like '%E';
+
+select *
+from employees
+where last_name like 'q%'
+and not last_name like 'qu%';
