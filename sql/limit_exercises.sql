49fa9a28b5a6be6733b14b2b5280e1b428347987
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-09-02 10:32:07 -0500

    limit ex

--- sql/limit_exercises.sql
+++ sql/limit_exercises.sql
@@ -1,7 +1,14 @@
-select distinct last_name from employees
+select distinct last_name
+from employees
 order by last_name desc
 limit 10;
 
-select distinct salary from salaries
+select emp_no
+from salaries
 order by salary desc
-limit 5
+limit 5;
+
+select emp_no
+from salaries
+order by salary desc
+limit 5 offset 45;
