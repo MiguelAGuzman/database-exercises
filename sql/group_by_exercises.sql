802733d7bf635bc3f848c5f23a149021f14aa919
MiguelAGuzman <miguel.a.guzman0009@gmail.com>
2022-09-05 15:38:42 -0500

    group by ex cont

--- sql/group_by_exercises.sql
+++ sql/group_by_exercises.sql
@@ -13,8 +13,15 @@ where last_name like 'E%'
   and last_name like '%E'
 group by last_name, first_name;
 
-select distinct last_name, first_name
+select last_name
 from employees
-where last_name like 'E%'
-  and last_name like '%E'
-group by last_name, first_name;
+where last_name like '%q%'
+  and not last_name like '%qu%'
+group by last_name;
+
+select count(*) as count_employees, last_name
+from employees
+where last_name like '%q%'
+  and not last_name like '%qu%'
+group by last_name
+order by count_employees;
